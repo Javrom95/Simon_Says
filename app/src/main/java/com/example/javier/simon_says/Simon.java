532b1a86f4e408b6bc5cdbc69f6cc8ac1fd987cd
javier <javrom95@gmail.com>
2016-05-10 16:29:04 +0200

    Commit number 5.

--- app/src/main/java/com/example/javier/simon_says/Simon.java
+++ app/src/main/java/com/example/javier/simon_says/Simon.java
@@ -1,6 +1,7 @@
 package com.example.javier.simon_says;
 
 import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.DrawableRes;
@@ -18,11 +19,9 @@ import java.util.Random;
 
 public class Simon extends AppCompatActivity {
 
-    Button btn1, btn2, btn3, btn4;
+    Button btn1, btn2, btn3, btn4, btnmrk;
     ArrayList<Integer> sequence = new ArrayList<Integer>();
-    int trans, add, rounds=4;
-    long time= 3000;
-    Handler handler = new Handler();
+    int trans, add, rounds=4, counter=0;
     Random r = new Random();
    /*
     */
@@ -38,97 +37,79 @@ public class Simon extends AppCompatActivity {
         btn2 = (Button) findViewById(R.id.button2);
         btn3 = (Button) findViewById(R.id.button3);
         btn4 = (Button) findViewById(R.id.button4);
-
+        btnmrk = (Button) findViewById(R.id.button);
         final MediaPlayer audiobutton1 = MediaPlayer.create(this, R.raw.button1);
         final MediaPlayer audiobutton2 = MediaPlayer.create(this, R.raw.button2);
         final MediaPlayer audiobutton3 = MediaPlayer.create(this, R.raw.button3);
         final MediaPlayer audiobutton4 = MediaPlayer.create(this, R.raw.button4);
 
-        //createSequence();
-
+        createSequence();
 
     }
 
     public void createSequence(){
         for(byte i=0;i<rounds;i++){
-            handler.postDelayed(new Runnable() {
-                public void run() {
                     add=r.nextInt((rounds - 1) + 1) + 1;
-
                     if(add==1){
                         //Reproduce sound.
-                        changeButton(add);
                         Log.i("Value:","1.");
                     }else if(add==2){
                         //Reproduce sound,
-                        changeButton(add);
                         Log.i("Value:","2.");
                     }else if(add==3){
                         //Reproduce sound.
-                        changeButton(add);
                         Log.i("Value:","3.");
                     }else{
                         //Reproduce sound.
-                        changeButton(add);
                         Log.i("Value:","4.");
                     }
-
                     sequence.add(add);
-                }
-            }, time);
         }
 
     }
 
 
+    public void repeatSequence(int button){
+        int i=0;
+        if(i<sequence.size()) {
+            if (button == sequence.get(i)) {
+                i++;
+                Log.i("Correct.","Correct value.");
+                Log.i("Position: ","Position "+i+" of "+sequence.size()+".");
+            } else {
 
-    public synchronized void changeButton(int rand){
-        if(add==1){
-
-            //Reproduce sound.
-            btn1.setBackgroundResource(R.color.colorpressedbutton1);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                public void run() {
-                    btn1.setBackgroundResource(R.drawable.button1_pressed);
-                }
-            }, time);
-            Log.i("Value:","1.");
-
-        }else if(add==2){
-            //Reproduce sound,
-            btn2.setBackgroundResource(R.color.colorpressedbutton2);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                public void run() {
-                    btn2.setBackgroundResource(R.drawable.button2_pressed);
-                }
-            }, time);
-            Log.i("Value:","2.");
-        }else if(add==3){
-            //Reproduce sound.
-            btn3.setBackgroundResource(R.color.colorpressedbutton3);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                public void run() {
-                    btn3.setBackgroundResource(R.drawable.button3_pressed);
-                }
-            }, time);
-            Log.i("Value:","3.");
-        }else{
-            //Reproduce sound.
-            btn4.setBackgroundResource(R.color.colorpressedbutton4);
-            Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                public void run() {
-                    btn4.setBackgroundResource(R.drawable.button4_pressed);
-                }
-            }, time);
-            Log.i("Value:","4.");
+            }
         }
-
-
+        if(i==sequence.size()){
+                i=0;
+                counter++;
+                //btnmrk.setText(counter);
+                Log.i("Correctly ended.","Correctly ended.");
+                rounds++;
+                Log.i("Sequence number:","1.");
+                createSequence();
+        }
     }
+
+
+    public void btn1Click(View view){
+        repeatSequence(1);
+        Log.i("Value pushed:","1.");
+    }
+    public void btn2Click(View view){
+        repeatSequence(2);
+        Log.i("Value pushed:","2.");
+    }
+    public void btn3Click(View view){
+        repeatSequence(3);
+        Log.i("Value pushed:","3");
+    }
+    public void btn4Click(View view){
+        repeatSequence(4);
+        Log.i("Value pushed:","4.");
+    }
+
+
 
  }
 
