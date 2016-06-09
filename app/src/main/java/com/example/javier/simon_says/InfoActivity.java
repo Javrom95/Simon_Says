39cc00b2d2e0170bfe31fae78423f6ade7f66301
javier <javrom95@gmail.com>
2016-06-09 17:19:27 +0200

    Completo 2.

--- app/src/main/java/com/example/javier/simon_says/InfoActivity.java
+++ app/src/main/java/com/example/javier/simon_says/InfoActivity.java
@@ -4,7 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+/**
+ * Created by javier on 23/05/16.
+ *
+ *
+ This file is part of Icemon Says
+ Icemon Says is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *
+ */
 public class InfoActivity extends AppCompatActivity {
 
     @Override
